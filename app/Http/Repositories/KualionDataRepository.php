bffca382b9a57ff698183966e37d6d8c73a56cce
SaulAlonso <saul.ibarra@cimat.mx>
2025-03-07 12:37:01 -0600

    UPDATE: add data export for contrapartesTable

--- app/Http/Repositories/KualionDataRepository.php
+++ app/Http/Repositories/KualionDataRepository.php
@@ -2,19 +2,63 @@
 
 namespace App\Http\Repositories;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class KualionDataRepository
 {
-    private $connection;
+    private $sourceConnection;
+    private $targetConnection;
+    private $teamId = 155;
+    private $startDate;
 
     public function __construct()
     {
-        $this->connection = DB::connection('oracle');
+        $this->sourceConnection = DB::connection('mysql');
+        $this->targetConnection = DB::connection('oracle');
+        $this->startDate = Carbon::now()->subWeek()->toDateString();
     }
 
     public function contrapartesTable()
     {
+        // Query origin data
+        $sourceData =  $this->sourceConnection->select(
+            sprintf(
+                "SELECT %s FROM %s where teamId = %s and created_at >= %s",
+                "alias, name, rfc, email, municipio, contractNumber, contractNumberId",
+                "enegence_cloud.invoiceRecipients",
+                $this->teamId,
+                $this->startDate,
+            )
+        );
+        // Parse to Array
+        $sourceDataArray = json_decode(json_encode($sourceData), true);
+
+        // Map to target database fields
+        $targetDataArray = array_map(
+            function ($item) {
+                return [
+                    'SHORTNAME'      => $item['alias'],
+                    'NAME'           => $item['name'],
+                    'RFC'            => $item['rfc'],
+                    'EMAIL'          => $item['email'],
+                    'MUNICIPIO'      => $item['municipio'],
+                    'CONTRACTNUMBER' => $item['contractNumber'],
+                    'CONTRACTNUMBERID' => $item['contractNumberId'],
+                ];
+            },
+            $sourceDataArray
+        );
+
+        // Parce to Chunks for optimization.
+        $chunks = array_chunk($targetDataArray, 1000);
+
+        // Run insert query in target connection transaction
+        DB::connection('oracle')->transaction(function () use ($chunks) {
+            foreach ($chunks as $chunk) {
+                $this->targetConnection->table('CONTRAPARTES')->insert($chunk);
+            }
+        });
     }
 
     public function diccionarioDeFoliosTable()
